fa1519ebdb51f2f5770f6856046da68c12fdd3bb
adilaf <100770370+adilaf@users.noreply.github.com>
2023-11-29 13:06:13 +0800

    edited slider

--- js/swiper.js
+++ js/swiper.js
@@ -1,13 +1,19 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
 function plusDivs(n) {
-  var sliderWrapper = document.querySelector('.slider-wrapper');
-  var slideWidth = sliderWrapper.scrollWidth / slides.length; // Width of each slide
-  slideIndex += n;
+    showDivs(slideIndex += n);
+}
 
-  if (slideIndex > slides.length) { slideIndex = 1; }
-  if (slideIndex < 1) { slideIndex = slides.length; }
+function showDivs(n) {
+    var slides = document.getElementsByClassName("slide");
+    var sliderWrapper = document.querySelector('.slider-wrapper');
+    var totalSlides = slides.length;
 
-  sliderWrapper.scrollTo({
-      left: slideWidth * (slideIndex - 1),
-      behavior: 'smooth'
-  });
+    if (n > totalSlides) { slideIndex = 1; }
+    if (n < 1) { slideIndex = totalSlides; }
+
+    var newTransformValue = -(slideIndex - 1) * 100; // In percentage
+
+    sliderWrapper.style.transform = 'translateX(' + newTransformValue + '%)';
 }
