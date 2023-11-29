21799c92c2caff5008152017223aa1976c6221a0
adilaf <100770370+adilaf@users.noreply.github.com>
2023-11-29 11:54:53 +0800

    added slider wrapper

--- js/swiper.js
+++ js/swiper.js
@@ -1,20 +1,16 @@
 
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("slide");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
+  var sliderWrapper = document.getElementById("sliderWrapper");
+
+  if (n > x.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = x.length; }
+  
+  // Calculate new margin
+  var newMarginLeft = -(slideIndex - 1) * 100 + '%';
+
+  // Apply new margin to slider wrapper
+  sliderWrapper.style.marginLeft = newMarginLeft;
 }
 
