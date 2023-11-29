4c5f434f58bc9c1dfb65a3c56d63455556d446c6
adilaf <100770370+adilaf@users.noreply.github.com>
2023-11-29 11:58:27 +0800

    edited swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,16 @@
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("slide");
-  var sliderWrapper = document.getElementById("sliderWrapper");
-
-  if (n > x.length) { slideIndex = 1; }
-  if (n < 1) { slideIndex = x.length; }
-  
-  // Calculate new margin
-  var newMarginLeft = -(slideIndex - 1) * 100 + '%';
-
-  // Apply new margin to slider wrapper
-  sliderWrapper.style.marginLeft = newMarginLeft;
+function updateSlidePosition() {
+  var slides = document.getElementsByClassName("slide");
+  for (var i = 0; i < slides.length; i++) {
+      slides[i].style.transform = 'translateX(' + (-slideIndex * 100) + '%)';
+  }
 }
 
+function showDivs(n) {
+  var totalSlides = document.getElementsByClassName("slide").length;
+  if (n > totalSlides) { slideIndex = 1; }
+  if (n < 1) { slideIndex = totalSlides; }
+  updateSlidePosition();
+}
+
+// Initialize the slider
+showDivs(slideIndex);
