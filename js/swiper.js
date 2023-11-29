2d8eed79c130a3bce8c69e1dee920ab4874c4de2
adilaf <100770370+adilaf@users.noreply.github.com>
2023-11-29 12:23:06 +0800

    edited the slider

--- js/swiper.js
+++ js/swiper.js
@@ -6,18 +6,14 @@ function plusDivs(n) {
 }
 
 function showDivs(n) {
-    var i;
     var slides = document.getElementsByClassName("slide");
-    var sliderWrapper = document.querySelector('.slider-wrapper'); // Corrected selector
+    var sliderWrapper = document.querySelector('.slider-wrapper');
     var totalSlides = slides.length;
 
     if (n > totalSlides) { slideIndex = 1; }
     if (n < 1) { slideIndex = totalSlides; }
 
-    // Calculate new margin
-    var newMarginLeft = -(slideIndex - 1) * 100; // This will be in percentage
+    var newTransformValue = -(slideIndex - 1) * 100; // In percentage
 
-    // Apply new margin to slider wrapper
-    sliderWrapper.style.transform = 'translateX(' + newMarginLeft + '%)'; // Using transform for sliding effect
+    sliderWrapper.style.transform = 'translateX(' + newTransformValue + '%)';
 }
-
