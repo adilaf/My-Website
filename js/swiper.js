43fd50acd7823d178ac7c82ec254069b25f72a47
adilaf <100770370+adilaf@users.noreply.github.com>
2023-11-29 12:07:08 +0800

    edited the slider

--- js/swiper.js
+++ js/swiper.js
@@ -1,21 +1,12 @@
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
 function showDivs(n) {
     var i;
     var slides = document.getElementsByClassName("slide");
+    var sliderWrapper = document.querySelector('.slider-wrapper');
     var totalSlides = slides.length;
 
-    if (n > totalSlides) { slideIndex = 1 }
-    if (n < 1) { slideIndex = totalSlides }
-    
-    for (i = 0; i < totalSlides; i++) {
-        slides[i].style.display = "none";
-    }
-    
-    slides[slideIndex-1].style.display = "flex";
+    if (n > totalSlides) { slideIndex = 1; }
+    if (n < 1) { slideIndex = totalSlides; }
+
+    var newLeft = -(slideIndex - 1) * 100; // 100% for each slide
+    sliderWrapper.style.transform = 'translateX(' + newLeft + '%)';
 }
