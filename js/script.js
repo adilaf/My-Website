e7a62992c4a8bc5793481e42910128b82149163c
adilaf <100770370+adilaf@users.noreply.github.com>
2023-12-15 22:30:44 +0800

    ADD DROPDOWN

--- js/script.js
+++ js/script.js
@@ -84,3 +84,11 @@ function showDesign() {
 }
 
 
+function toggleDropdown(id) {
+  var content = document.getElementById(id);
+  if (content.style.display === "block") {
+      content.style.display = "none";
+  } else {
+      content.style.display = "block";
+  }
+}
